2032b62e4148c15f79d36863599cf9e131a8fbc8
cema-sp <cema.rus@gmail.com>
2015-08-08 02:04:06 +0300

    Adding imagick/imagick

--- splitter.go
+++ splitter.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"net/http"
-	"log"
+	// "github.com/gographics/imagick/imagick"
 	"io/ioutil"
-  "regexp"
-  "strings"
-  "errors"
-  // "html/template"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+	// "html/template"
 )
 
 // type PostHandler struct {}
@@ -16,19 +17,19 @@ import (
 // func (h PostHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {}
 
 func matchContentType(ct []string, matching string) error {
-  ctJoined := strings.Join(ct, "; ")
-  matched, err := regexp.MatchString(matching+`;*\s*.*`, ctJoined)
-  if !matched {
-    if err != nil {
-      log.Println(err)
-    }
-    return errors.New(
-      fmt.Sprintf(
-        "Invalid Content-Type: %s,\nExpected: %s",
-        ctJoined,
-        matching))
-  }
-  return nil
+	ctJoined := strings.Join(ct, "; ")
+	matched, err := regexp.MatchString(matching+`;*\s*.*`, ctJoined)
+	if !matched {
+		if err != nil {
+			log.Println(err)
+		}
+		return errors.New(
+			fmt.Sprintf(
+				"Invalid Content-Type: %s,\nExpected: %s",
+				ctJoined,
+				matching))
+	}
+	return nil
 }
 
 func handlePost(rw http.ResponseWriter, req *http.Request) {
@@ -38,50 +39,66 @@ func handlePost(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-  if err = matchContentType(
-    req.Header["Content-Type"],
-    `multipart/form-data`); err != nil {
-    log.Println(err)
-    return
-  }
+	if err = matchContentType(
+		req.Header["Content-Type"],
+		`multipart/form-data`); err != nil {
+		log.Println(err)
+		return
+	}
 
 	if req.ContentLength <= 0 {
 		log.Println("Body length: 0")
-    return
+		return
 	}
 
-  file, fileHeader, err := req.FormFile("imageFile")
-  if err != nil {
-    log.Println(err)
-    return
-  }
+	file, fileHeader, err := req.FormFile("imageFile")
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-  if err = matchContentType(
-    fileHeader.Header["Content-Type"],
-    `image/jpeg`); err != nil {
-    log.Println(err)
-    return
-  }
+	// Validate file type
+	if err = matchContentType(
+		fileHeader.Header["Content-Type"],
+		`image/jpeg`); err != nil {
+		log.Println(err)
+		return
+	}
 
 	var image []byte
 	image, err = ioutil.ReadAll(file)
-    if err != nil {
-      log.Println(err)
-      return
-    }
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-    if len(image) == 0 {
-    	log.Println("Body length: 0")
-      return
-    }
+	// Validate true image type
+	if err = matchContentType(
+		[]string{http.DetectContentType(image)},
+		`image/jpeg`); err != nil {
+		log.Println(err)
+		return
+	}
 
-	log.Println("Body: ", len(image), http.DetectContentType(image))
+	// if len(image) == 0 {
+	// 	log.Println("Body length: 0")
+	//   return
+	// }
+
+	log.Println("Image: ", len(image), http.DetectContentType(image))
 }
 
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "form.html")
-  })
-  http.HandleFunc("/split", handlePost)
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	// imagick.Initialize()
+	// defer imagick.Terminate()
+
+	// mw := imagick.NewMagickWand()
+	// defer mw.Destroy()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "form.html")
+	})
+	http.HandleFunc("/split", handlePost)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
