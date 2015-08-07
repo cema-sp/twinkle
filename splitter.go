d447a6fe2fecf1bb3211ead3c41d3a68290131d6
cema-sp <cema.rus@gmail.com>
2015-08-07 23:11:48 +0300

    Moving Content-Type validation to function

--- splitter.go
+++ splitter.go
@@ -11,6 +11,22 @@ import (
 
 // func (h PostHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {}
 
+func matchContentType(ct []string, matching string) error {
+  ctJoined := strings.Join(ct, "; ")
+  matched, err := regexp.MatchString(matching+`;*\s*.*`, ctJoined)
+  if !matched {
+    if err != nil {
+      log.Println(err)
+    }
+    return errors.New(
+      fmt.Sprintf(
+        "Invalid Content-Type: %s,\nExpected: %s",
+        ctJoined,
+        matching))
+  }
+  return nil
+}
+
 func handlePost(rw http.ResponseWriter, req *http.Request) {
 	var err error
 	if req.Method != "POST" {
@@ -18,32 +34,33 @@ func handlePost(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	err = http.ErrNotMultipart
-	for _, v := range req.Header["Content-Type"] {
-		if v == "multipart/form-data" {
-			err = nil
-		}
-	}
-	if err != nil {
-		log.Println(err)
-		return
-	}
+  if err = matchContentType(
+    req.Header["Content-Type"],
+    `multipart/form-data`); err != nil {
+    log.Println(err)
+    return
+  }
 
 	if req.ContentLength <= 0 {
 		log.Println("Body length: 0")
-        return
+    return
 	}
 
-	var body []byte
-	body, err = ioutil.ReadAll(req.Body)
+
+  if err = matchContentType(
+    fileHeader.Header["Content-Type"],
+    `image/jpeg`); err != nil {
+    log.Println(err)
+    return
+  }
     if err != nil {
-        log.Println(err)
-        return
+      log.Println(err)
+      return
     }
 
     if len(body) == 0 {
     	log.Println("Body length: 0")
-        return
+      return
     }
 
 	log.Println(string(body))
